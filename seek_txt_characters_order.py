a1293546264edbaa179a86276c1dcf6ecc34418f
Huangongshu <44497562+Huangongshu@users.noreply.github.com>
2019-08-20 15:13:37 +0800

    Update seek_txt_characters_order.py

--- seek_txt_characters_order.py
+++ seek_txt_characters_order.py
@@ -78,43 +78,39 @@ def num_sum(df,im_path,type_num_str):
     r_1_num=count_num(im_path1,r_1_df,'R')
     return l_1_num+r_1_num
 
-df=pd.read_excel('D:/第一和第二批2018和2019.xlsx')
+if __name__=='__main__':
+    df=pd.read_excel('D:/第一和第二批2018和2019.xlsx')
+    df['期']=df.影像诊断.str.extract('糖尿病视网膜病变[^0-9](\d)期',expand=False).fillna('0')
+    df=df.drop(df.columns[0],axis=1)
+    df1=pd.DataFrame(columns={'挂号号码','眼睛','期'})
 
-#df=df[((df['影像诊断'].str.rfind('糖尿病视网膜病变')!=-1))&(df['影像诊断'].str.rfind('期')!=-1)&(df['影像诊断'].str.rfind('可疑')==-1)]
-#df=pd.concat([df,pd.DataFrame(columns={'期'}).fillna(0)]).drop(df.columns[0],axis=1)
-df['期']=df.影像诊断.str.extract('糖尿病视网膜病变[^0-9](\d)期',expand=False).fillna('0')
-#df['影像诊断']=df.影像诊断.str.replace('糖尿病视网膜病变[^0-9](\d)期',expand=False)
+    for i,n in zip(df.index,df['影像诊断']):
+        if ('Ⅰ' in n) or ('糖尿病视网膜病变（非增殖，3期）' in n)or('糖尿病视网膜病变（非增殖，3期）' in n):
+            df['期'][i]='1'
+        elif ('Ⅱ'in n) or ('糖尿病视网膜病变2期' in n):
+            df['期'][i]='2'
+        elif ('Ⅲ' in n) or ('糖尿病视网膜病变（非增殖，3期）' in n):
+            df['期'][i]='3'
+        elif 'Ⅳ' in n:
+            df['期'][i]='4'
+        elif 'Ⅴ' in n:
+            df['期'][i]='5'
+        elif 'Ⅵ' in n:
+            df['期'][i]='6'             
 
-df=df.drop(df.columns[0],axis=1)
-df1=pd.DataFrame(columns={'挂号号码','眼睛','期'})
+    pattern='糖尿病视网膜病变'
 
-for i,n in zip(df.index,df['影像诊断']):
-    if ('Ⅰ' in n) or ('糖尿病视网膜病变（非增殖，3期）' in n)or('糖尿病视网膜病变（非增殖，3期）' in n):
-        df['期'][i]='1'
-    elif ('Ⅱ'in n) or ('糖尿病视网膜病变2期' in n):
-        df['期'][i]='2'
-    elif ('Ⅲ' in n) or ('糖尿病视网膜病变（非增殖，3期）' in n):
-        df['期'][i]='3'
-    elif 'Ⅳ' in n:
-        df['期'][i]='4'
-    elif 'Ⅴ' in n:
-        df['期'][i]='5'
-    elif 'Ⅵ' in n:
-        df['期'][i]='6'             
+    for i in range(len(df)):
+        df1=matching_information(df,df1,i)
 
-pattern='糖尿病视网膜病变'
+    im_path1='D:/huan/第二批数据过滤/abnormal2'
+    im_path2='D:/huan/第二批数据过滤/normal2'
 
-for i in range(len(df)):
-    df1=matching_information(df,df1,i)
-    
-im_path1='D:/huan/第二批数据过滤/abnormal2'
-im_path2='D:/huan/第二批数据过滤/normal2'
+    type_1_num=num_sum(df1,im_path1,'1')+num_sum(df1,im_path2,'1')
+    type_2_num=num_sum(df1,im_path1,'2')+num_sum(df1,im_path2,'2')
+    type_3_num=num_sum(df1,im_path1,'3')+num_sum(df1,im_path2,'3')
+    type_4_num=num_sum(df1,im_path1,'4')+num_sum(df1,im_path2,'4')
+    type_5_num=num_sum(df1,im_path1,'5')+num_sum(df1,im_path2,'5')
+    type_6_num=num_sum(df1,im_path1,'6')+num_sum(df1,im_path2,'6')
 
-type_1_num=num_sum(df1,im_path1,'1')+num_sum(df1,im_path2,'1')
-type_2_num=num_sum(df1,im_path1,'2')+num_sum(df1,im_path2,'2')
-type_3_num=num_sum(df1,im_path1,'3')+num_sum(df1,im_path2,'3')
-type_4_num=num_sum(df1,im_path1,'4')+num_sum(df1,im_path2,'4')
-type_5_num=num_sum(df1,im_path1,'5')+num_sum(df1,im_path2,'5')
-type_6_num=num_sum(df1,im_path1,'6')+num_sum(df1,im_path2,'6')
-
-sum_num=type_1_num+type_2_num+type_3_num+type_4_num+type_5_num+type_6_num
+    sum_num=type_1_num+type_2_num+type_3_num+type_4_num+type_5_num+type_6_num
